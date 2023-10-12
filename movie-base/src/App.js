45178b114eba2457c5eacef7a2629198efdd4616
Karan Meghe <karanmeghe87@gmail.com>
2023-10-12 20:22:33 +0530

    Add - Using composition to make reusable box

--- movie-base/src/App.js
+++ movie-base/src/App.js
@@ -1,23 +1,29 @@
 import { useState } from "react";
 import Body from "./Components/layout/Body";
 import Navbar from "./Components/layout/Navbar";
-import { tempMovieData } from "./Components/utils/config";
-import MoviesListBox from "./Components/Movies/MoviesListBox";
+import { tempMovieData, tempWatchedData } from "./Components/utils/config";
+import Box from "./Components/Movies/Box";
 import MoviesList from "./Components/Movies/MoviesList";
-import MoviesWatchedListBox from "./Components/Movies/MoviesWatchedListBox";
+import MoviesSummary from "./Components/Movies/MoviesSummary";
+import MovieWatched from "./Components/Movies/MovieWatched";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
       <Navbar moviesList={[movies, setMovies]} />
 
       <Body>
-        <MoviesListBox>
+        <Box>
           <MoviesList moviesList={[movies, setMovies]} />
-        </MoviesListBox>
-        <MoviesWatchedListBox />
+        </Box>
+
+        <Box>
+          <MoviesSummary summary={[watched, setWatched]} />
+          <MovieWatched summary={[watched, setWatched]} />
+        </Box>
       </Body>
     </>
   );
